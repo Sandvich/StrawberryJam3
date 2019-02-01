6e465cb67d83ba289c19563aaf6ee7879115c4e8
Sanchit Sharma <sanchitsharma1@gmail.com>
2019-02-01 21:43:59 +0000

    Updated project settings

--- src/Conductor.gd
+++ src/Conductor.gd
@@ -1,13 +1,19 @@
 extends AudioStreamPlayer
 
 var time = 0.0
+var label
+var time_decimal = [0, 0]
+var time_string = "%dm %ds"
 
 func _ready():
 	set_process(false)
+	label = get_parent().get_node("time")
 
 func _process(delta):
 	time += delta
-	print(time)
+	time_decimal[0] = floor(time/60)
+	time_decimal[1] = floor(fmod(time, 60))
+	label.set_text(time_string % time_decimal)
 
 func start():
 	set_process(true)
