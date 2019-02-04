324d74798b30ff77edcf3f350e7ea9e27f01e58b
Sanchit Sharma <sanchitsharma1@gmail.com>
2019-02-04 14:48:17 +0000

    Moved keymap definitions to the joy_changed function, to minimise the amount that needs to be loaded from disk.

--- src/code/Deck.gd
+++ src/code/Deck.gd
@@ -1,61 +1,59 @@
 extends AnimatedSprite
 
-var playstation_map
-var xbox_map
-var keyboard_map
 var key_map
 var valid_keys
 var promptClass
 
 func _on_joy_connection_changed(device_id, connected):
+	var keyboard = false
 	if connected:
 		var name = Input.get_joy_name(device_id)
 		print(name)
 		if "DualShock" in name:
 			print("Switching to playstation mapping.")
-			key_map = playstation_map
+			key_map = {
+				"left_deck_up":load("res://assets/w.png"),
+				"left_deck_left":load("res://assets/a.png"),
+				"left_deck_down":load("res://assets/s.png"),
+				"left_deck_right":load("res://assets/d.png"),
+				"right_deck_left":load("res://assets/arrow_left.png"),
+				"right_deck_right":load("res://assets/arrow_right.png"),
+				"right_deck_up":load("res://assets/arrow_up.png"),
+				"right_deck_down":load("res://assets/arrow_down.png")
+			}
 		elif "XBox" in name:
 			print("Switching to xbox mapping.")
-			key_map = xbox_map
+			key_map = {
+				"left_deck_up":load("res://assets/w.png"),
+				"left_deck_left":load("res://assets/a.png"),
+				"left_deck_down":load("res://assets/s.png"),
+				"left_deck_right":load("res://assets/d.png"),
+				"right_deck_left":load("res://assets/arrow_left.png"),
+				"right_deck_right":load("res://assets/arrow_right.png"),
+				"right_deck_up":load("res://assets/arrow_up.png"),
+				"right_deck_down":load("res://assets/arrow_down.png")
+			}
 		else:
 			print("Unknown device, defaulting to keyboard mapping.")
-			key_map = keyboard_map
+			keyboard=true
 	else:
 		print("Keyboard")
-		key_map = keyboard_map
+		keyboard=true
+	
+	if keyboard:
+		key_map = {
+			"left_deck_up":load("res://assets/w.png"),
+			"left_deck_left":load("res://assets/a.png"),
+			"left_deck_down":load("res://assets/s.png"),
+			"left_deck_right":load("res://assets/d.png"),
+			"right_deck_left":load("res://assets/arrow_left.png"),
+			"right_deck_right":load("res://assets/arrow_right.png"),
+			"right_deck_up":load("res://assets/arrow_up.png"),
+			"right_deck_down":load("res://assets/arrow_down.png")
+		}
 
 func _ready():
 	Input.connect("joy_connection_changed", self, "_on_joy_connection_changed")
-	keyboard_map = {
-		"left_deck_up":load("res://assets/w.png"),
-		"left_deck_left":load("res://assets/a.png"),
-		"left_deck_down":load("res://assets/s.png"),
-		"left_deck_right":load("res://assets/d.png"),
-		"right_deck_left":load("res://assets/arrow_left.png"),
-		"right_deck_right":load("res://assets/arrow_right.png"),
-		"right_deck_up":load("res://assets/arrow_up.png"),
-		"right_deck_down":load("res://assets/arrow_down.png")
-	}
-	playstation_map = {
-		"left_deck_up":load("res://assets/w.png"),
-		"left_deck_left":load("res://assets/a.png"),
-		"left_deck_down":load("res://assets/s.png"),
-		"left_deck_right":load("res://assets/d.png"),
-		"right_deck_left":load("res://assets/arrow_left.png"),
-		"right_deck_right":load("res://assets/arrow_right.png"),
-		"right_deck_up":load("res://assets/arrow_up.png"),
-		"right_deck_down":load("res://assets/arrow_down.png")
-	}
-	xbox_map = {
-		"left_deck_up":load("res://assets/w.png"),
-		"left_deck_left":load("res://assets/a.png"),
-		"left_deck_down":load("res://assets/s.png"),
-		"left_deck_right":load("res://assets/d.png"),
-		"right_deck_left":load("res://assets/arrow_left.png"),
-		"right_deck_right":load("res://assets/arrow_right.png"),
-		"right_deck_up":load("res://assets/arrow_up.png"),
-		"right_deck_down":load("res://assets/arrow_down.png")
-	}
 	
 	var devices = Input.get_connected_joypads()
 	if devices.size() > 0:
